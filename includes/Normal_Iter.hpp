3230a747de346efaf8f6adcdb6da90d6b37f168a
Youkjin Lee <ukjinlee66@gmail.com>
2021-04-08 18:22:24 +0900

    List yet

--- includes/Normal_Iter.hpp
+++ includes/Normal_Iter.hpp
@@ -16,7 +16,6 @@
 #include <List>
 namespace ft
 {
-    std::list<int>::iterator vec;
     template<typename Category, typename T>
     struct  IteratorTrait
     {
@@ -40,25 +39,172 @@ namespace ft
             typedef N                                   node_type;
             typedef node_type*                          node_pointer;
         protected:
-            node_pointer                                p;
+            node_pointer                                cur;
         public:
             NormalIter() : cur(NULL){}
-            NormalIter(NormalIter const *no) : cur(no->cur) {}
-            T &operator*()
+            NormalIter(NormalIter const &no) : cur(no.cur) {}
+            NormalIter(node_pointer p) : cur(p) {}
+            virtual ~NormalIter() {}
+
+            node_pointer &ptr()
             {
-                return (cur->val);
+                return (this->cur);
             }
-            bool operator==(const NormalIter &no)
+            node_pointer get_node() const
             {
-                return (cur == no.cur);
+                return (this->cur);
             }
-            bool operator!=(const NormalIter &no)
+            reference operator*()
             {
-                return (cur != no.cur);
+                return (this->cur->val);
             }
-            virtual ~NormalIter(){}
+            const_reference operator*()
+            {
+                return (this->cur->val);
+            }
+            pointer operator->()
+            {
+                return (&this->cur->val);
+            }
+            const_pointer operator->() const
+            {
+                return (&this->cur->val);
+            }
+            NormalIter &operator++()
+            {
+                this->cur = this->cur->next;
+                return (*this);
+            }
+            NormalIter operator++(int)
+            {
+                NormalIter temp(*this);
+                this->cur = this->cur->next;
+                return (temp);
+            }
+            NormalIter &operator--()
+            {
+                this->cur = this->cur->prev;
+                return (*this);
+            }
+            NormalIter operator--(int)
+            {
+                NormalIter temp(*this);
+                this->cur = this->cur->prev;
+                return (temp);
+            }
+            NormalIter &operator+=(int val)
+            {
+                if (val > 0)
+                {
+                    for(int i=0;i<val;i++)
+                        this->cur = this->cur->next;
+                }
+                else
+                {
+                    for(int i= val;i>0;i--)
+                        this->cur = this->cur->prev;
+                }
+            }
+            NormalIter operator+(int val) const
+            {
+                NormalIter temp(*this);
+                return (temp += val);
+            }
+            NormalIter &operator-=(int val)
+            {
+                operator+=(-val);
+                return (*this);
+            }
+            NormalIter operator-(int val) const
+            {
+                NormalIter temp(*this);
+                return (temp -= val);
+            }
+            bool operator==(NormalIter const &no)
+            {
+                return (this->cur == no.cur);
+            }
+            bool operator!=(NormalIter const &no)
+            {
+                return (this->cur != no.cur);
+            }
+            bool operator<(NormalIter const &no)
+            {
+                return (this->cur < no.cur);
+            }
+            bool operator<=(NormalIter const &no)
+            {
+                return (this->cur <= no.cur);
+            }
+            bool operator>(NormalIter const &no)
+            {
+                return (this->cur > no.cur);
+            }
+            bool operator>=(NormalIter const &no)
+            {
+                return (this->cur >= no.cur);
+            }
             
     };
+    template <typename It>
+    class ReverseNormalIter : public It
+    {
+        public:
+            using typename it::value_type;
+            using typename It::pointer;
+            using typename It::const_pointer;
+            using typename It::reference;
+            using typename It::const_reference;
+            using typename It::difference_type;
+        public:
+            ReverseNormalIter(): It() {}
+            ReverseNormalIter(It const &it) : It(it) {}
+            ReverseNormalIter(ReverseNormalIter const &re) : It(re.p) {}
+            ReverseNormalIter &operator=(ReverseNormalIter const &re)
+            {
+                this->p = re.p;
+                return (*this);
+            }
+            ReverseNormalIter operator*()
+            {
+                It temp(*this);
+                return (*--temp);
+            }
+            const_reference operator*() const
+            {
+                It temp(*this);
+                return (*--temp);
+            }
+            pointer operator->()
+            {
+                It temp(*this);
+                return (&*--temp);
+            }
+            const_pointer operator->() const
+            {
+                It temp(*this);
+                return (&*--temp);
+            }
+            ReverseNormalIter operator++(int)
+            {
+                ReverseNormalIter temp(*this);
+                operator++();
+                return (temp);
+            }
+            It &operator++()
+            {
+                return (this->It::operator--());
+            }
+            ReverseNormalIter operator--(int)
+            {
+                ReverseNormalIter temp(*this);
+                operator--();
+                return (temp);
+            }
+            It &operator--()
+            {
+                return (this->It::operator++());
+            }
+    };
 }
-
 #endif
