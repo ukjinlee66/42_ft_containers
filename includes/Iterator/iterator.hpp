7da923faaa270eac21a967ec323d02222c9b7ee8
Youkjin Lee <ukjinlee66@gmail.com>
2021-07-08 19:22:51 +0900

    yet

--- includes/Iterator/iterator.hpp
+++ includes/Iterator/iterator.hpp
@@ -12,6 +12,7 @@
 
 #ifndef ITERATOR_HPP
 #define ITERATOR_HPP
+typedef int ptrdiff_t;
 namespace ft
 {
 	template<class Category, class T, class Distance = ptrdiff_t,
@@ -19,16 +20,44 @@ namespace ft
 	class iterator
 	{
 		public:
-				typedef typename Distance	difference_type;
-				typedef typename T			value_type;
-				typedef typename T*			pointer;
-				typedef typename T&			reference;
-				typedef typename Category	iterator_category;
+				typedef typename Distance						difference_type;
+				typedef typename T								value_type;
+				typedef typename T*								pointer;
+				typedef typename T&								reference;
+				typedef typename Category						iterator_category;
 	};
 }
 //iterator_traits
 namespace ft
 {
+	template <class iterator> 
+	class iterator_traits
+	{
+		public:
+				typedef typename iterator::difference_type		difference_type;
+				typedef typename iterator::value_type			value_type;
+				typedef typename iterator::pointer				pointer;
+				typedef typename iterator::reference			reference;
+				typedef typename iterator::iterator_category	iterator_category;
+	};
+	template <class T> class iterator_traits<T*>
+	{
+		public:
+				typedef typename ptrdiff_t						difference_type;
+				typedef typename T								value_type;
+				typedef typename T*								pointer;
+				typedef typename T&								reference;
+				typedef typename random_access_iterator_tag		iterator_category;
 
+	};
+	template <class T> class iterator_traits<const T*>
+	{
+		public:
+				typedef typename ptrdiff_t						difference_type;
+				typedef typename T								value_type;
+				typedef typename const T*						pointer;
+				typedef typename const T&						reference;
+				typedef typename random_access_iterator_tag		iterator_category;
+	};
 }
 #endif
