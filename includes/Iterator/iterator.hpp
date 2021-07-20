a0b28cd27b4d881c8118d2998d33eac496cc48a1
Youkjin Lee <ukjinlee66@gmail.com>
2021-07-20 14:10:33 +0900

    reverse Iterator Operator yet

--- includes/Iterator/iterator.hpp
+++ includes/Iterator/iterator.hpp
@@ -25,10 +25,7 @@ namespace ft
 				typedef typename T&								reference;
 				typedef typename Category						iterator_category;
 	};
-}
-//iterator_traits
-namespace ft
-{
+	//iterator_traits
 	template <class iterator> 
 	class iterator_traits
 	{
@@ -58,73 +55,129 @@ namespace ft
 				typedef typename const T&						reference;
 				typedef typename random_access_iterator_tag		iterator_category;
 	};
-struct input_iterator_tag
-{      
-// identifying tag for input iterators
-};
- 
-struct output_iterator_tag
-{      
-// identifying tag for output iterators
-};
- 
-struct forward_iterator_tag
-    : public input_iterator_tag, output_iterator_tag
-{      
-// identifying tag for forward iterators
-};
- 
-struct bidirectional_iterator_tag
-    : public forward_iterator_tag
-{      
-// identifying tag for bidirectional iterators
-};
- 
-struct random_access_iterator_tag
-    : public bidirectional_iterator_tag
-{      
-// identifying tag for random-access iterators
-};
-
-// iterator distance base function overloading
-template<class InputIterator> typename iterator_traits<InputIterator>::difference_type
-distance (InputIterator first, InputIterator last)
-{
+	struct input_iterator_tag
+	{      
+	// identifying tag for input iterators
+	};
 	
-}
+	struct output_iterator_tag
+	{      
+	// identifying tag for output iterators
+	};
+	
+	struct forward_iterator_tag
+		: public input_iterator_tag, output_iterator_tag
+	{      
+	// identifying tag for forward iterators
+	};
+	
+	struct bidirectional_iterator_tag
+		: public forward_iterator_tag
+	{      
+	// identifying tag for bidirectional iterators
+	};
+	
+	struct random_access_iterator_tag
+		: public bidirectional_iterator_tag
+	{      
+	// identifying tag for random-access iterators
+	};
 
-// iterator advance base function overloading
-// Input iterator tag
-template <class InputIterator, class Distance>
-void advance (InputIterator& it, Distance n)
-{
+	// iterator distance base function overloading
 
-}
-// Output Iterator tag
-template <class OutputIterator, class Distance>
-void advance (OutputIterator& it, Distance n)
-{
+	/*
+	*	Calculates the number of elements between first and last.
+	*	If it is a random-access iterator, the function uses operator- to calculate this.
+	*	Otherwise, the function uses the increase operator (operator++) repeatedly.
+	*/
 
-}
-// forward Iterator tag
-template <class ForwardIterator, class Distance>
-void advance (ForwardIterator& it, Distance n)
-{
+	// randomaccess iterator
+	template<class RandomaccessIterator> typename iterator_traits<RandomaccessIterator>::difference_type
+	distance (RandomaccessIterator first, RandomaccessIterator last, random_access_iterator_tag)
+	{
+		return (last - first);
+	}
 
-}
-// bidirectional iterator tag
-template <class BidirectionalIterator, class Distance>
-void advance (BidirectionalIterator& it, Distance n)
-{
+	// Nonrandomaccess iterator
+	template<class NonRandomaccessIterator> typename iterator_traits<NonRandomaccessIterator>::difference_type
+	distance (NonRandomaccessIterator first, NonRandomaccessIterator last, input_iterator_tag)
+	{
+		iterator_traits<NonRandomaccessIterator>::difference_type dist = 0;
+		for(;first!=last;first++)
+			dist++;
+		return (dist);
+	}
 
-}
-// randomaccess iterator tag
-template <class RandomaccessIterator, class Distance>
-void advance (RandomaccessIterator& it, Distance n)
-{
+	// default distance method
+	template<class InputIterator> typename iterator_traits<InputIterator>::difference_type
+	distance (InputIterator first, InputIterator last)
+	{
+		return distance(first, last, iterator_traits<InputIterator>::iterator_category());	
+	}
 
-}
-// iterator distance base function overloading
+	// iterator advance base function overloading
 
-};//end namespace
+	/*
+	*	Advances the iterator it by n element positions.
+	*	If it is a random-access iterator, the function uses just once operator+ or operator-. 
+	*	Otherwise, the function uses repeatedly the increase or decrease operator 
+	*	(operator++ or operator--) until n elements have been advanced.
+	*/
+
+	// randomaccess iterator
+	template <class RandomaccessIterator, class Distance>
+	void advance (RandomaccessIterator& it, Distance n, random_access_iterator_tag)
+	{
+		it += n;
+	}
+
+	// non randomaccess iterator tag
+	template <class NonRandomaccessIterator, class Distance>
+	void advance (NonRandomaccessIterator& it, Distance n, input_iterator_tag)
+	{
+		for (ptrdiff_t i = 0; i < n; i++)
+			++it;
+	}
+
+	//default advance method
+	template <class InputIterator, class Distance>
+	void advance (InputIterator& it, Distance n)
+	{
+		return advance(it, n, typename iterator_traits<InputIterator>::iterator_category());
+	}
+
+	// reverse iterator
+	template <class Iterator> class reverse_iterator
+	{
+		private:
+				// Parameter
+				Iterator ite;
+				//default Constructs
+				reverse_iterator();
+		public:
+				// Member type
+				typedef Iterator iterator_type;
+				typedef iterator_traits<Iterator>::iterator_category iterator_category;
+				typedef iterator_traits<Iterator>::value_type value_type;
+				typedef iterator_traits<Iterator>::difference_type difference_type;
+				typedef iterator_traits<Iterator>::pointer pointer;
+				typedef iterator_traits<Iterator>::reference reference;
+
+				// Member functions
+				//initialization
+				explicit reverse_iterator (iterator_type it) : ite(it) {}
+				//copy
+				template <class Iter> reverse_iterator (const reverse_iterator<Iter>& rev_it) : ite(rev_it.ite) {}
+				//default Deconstructor
+				~reverse_iterator();
+
+				Iterator base() const
+				{
+					return (ite);
+				}
+
+				// Operator
+				
+	};
+}//end namespace
 #endif
